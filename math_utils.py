fc8369220dee3abbf67cf577a59d1c50709a81e8
tomerer1415-web <tomerer1415@gmail.com>
2025-10-30 13:33:10 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,5 +1,15 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    if num1 > num2 :
+      if num1 > num3 :
+        return num1 
+      else : 
+        return num3
+    else : 
+      if num2 > num3 : 
+        return num2 
+      else : 
+        return num3
+    
 
 def find_mean(num1, num2, num3):
     pass  # Replace 'pass' with code
