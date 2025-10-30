ce82dd9e7ea366fee96db5472ad2283ad0542fb8
tomerer1415-web <tomerer1415@gmail.com>
2025-10-30 13:45:21 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -18,5 +18,8 @@ def find_mean(num1, num2, num3):
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+    std = ((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean )**2)/3**0.5
+    return mean, std 
+    
+    
 
